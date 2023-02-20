859ef03435dc659c57e37c4e7523385e603f69d2
Álvaro <ainiguezdislaa@gmail.com>
2023-02-20 17:57:33 +0100

    ej terminado

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,8 @@
-package aplicacion;public class Principal {
+package aplicacion;
+
+public class Principal {
+    public static void main(String[] args) {
+        int[] lista = {1,2,3,4,5,6,7,8,9,10};
+        System.out.println("La media es: " + dominio.ej2_5_lambda.media(lista));
+    }
 }
--- src/dominio/ej2_5_lambda.java
+++ src/dominio/ej2_5_lambda.java
@@ -1,2 +1,7 @@
-package dominio;public class ej2_5_lambda {
+package dominio;
+import java.util.stream.Stream;
+public class ej2_5_lambda {
+    public static int media(int[] lista){
+        return Stream.iterate(0,i->i+1).limit(lista.length).map(i->lista[i]).reduce(0,(a,b)->a+b)/ lista.length;
+    }
 }
